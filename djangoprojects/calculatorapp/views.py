3f324107ea7c1a67f7906497fc2f4bed247d8dec
alirezanobakht2004 <alireza.nobakht2004@gmail.com>
2023-09-15 14:29:25 +0330

    calculator completed

--- djangoprojects/calculatorapp/views.py
+++ djangoprojects/calculatorapp/views.py
@@ -4,3 +4,21 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'index.html')
+
+def submitquery(request):
+    q = request.GET['query']
+    try:
+        ans = eval(q)
+        mydictionary = {
+            "q" : q,
+            "ans" : ans,
+            "error" : False,
+            "result" : True
+        }
+        return render(request,'index.html',context=mydictionary)
+    except:
+        mydictionary = {
+            "error" : True,
+            "result" : False
+        }
+        return render(request,'index.html',context=mydictionary)
